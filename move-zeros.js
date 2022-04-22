14b2d7873739bf8bde58fba801f35492d9320a8b
Burak Can Karasel <burakcankarasel@gmail.com>
2022-04-22 18:35:18 +0300

    move zeros app completed

---
+++ move-zeros.js
@@ -0,0 +1,10 @@
+function moveZeros(arr) {
+    for(i of arr){
+        if(i === 0){
+            let index = arr.indexOf(i)
+            arr.splice(index, 1)
+            arr.push(0)
+        }
+    }
+    return arr
+  }
