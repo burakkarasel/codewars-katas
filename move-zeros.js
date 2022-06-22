87ecb0a26a471bb8f05a7c20eab9383b9ff1ae00
kullop <burakcankarasel@gmail.com>
2022-06-22 23:15:37 +0300

    Changed solution for Move Zeros kata"

--- move-zeros.js
+++ move-zeros.js
@@ -1,4 +1,9 @@
 function moveZeros(arr) {
+    return arr.filter(item => item !== 0).concat(arr.filter(item => item === 0))
+}
+
+// obsolote
+/* function moveZeros(arr) {
     for(i of arr){
         if(i === 0){
             let index = arr.indexOf(i)
@@ -7,4 +12,4 @@ function moveZeros(arr) {
         }
     }
     return arr
-  }
+  } */
