9783bd0437c2d1be644bf9397bafaae9ad3bdc0b
kullop <burakcankarasel@gmail.com>
2022-06-25 15:22:25 +0300

    is string uppercase kata solution completed

---
+++ is-string-uppercase.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"strings"
+)
+
+type MyString string
+
+func (s MyString) IsUpperCase() bool {
+	return s == MyString(strings.ToUpper(string(s)))
+}
+
+// obsolote
+/* func (s MyString) IsUpperCase() bool {
+	for _, char := range s {
+		if char >= 97 && char <= 122 {
+			return false
+		}
+	}
+	return true
+} */
