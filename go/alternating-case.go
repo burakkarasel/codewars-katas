31a78e430ea42f3f590a9913fdc1426307bfe671
kullop <burakcankarasel@gmail.com>
2022-06-25 20:27:27 +0300

    altERnaTIng cAsE <=> ALTerNAtiNG CaSe kata solution completed

---
+++ go/alternating-case.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"strings"
+	"unicode"
+)
+
+func ToAlternatingCase(str string) string {
+	slc := strings.Split(str, "")
+	for i, v := range str {
+		if unicode.IsUpper(v) == true {
+			slc[i] = strings.ToLower(slc[i])
+		} else if unicode.IsLower(v) {
+			slc[i] = strings.ToUpper(slc[i])
+		}
+	}
+	return strings.Join(slc, "")
+}
