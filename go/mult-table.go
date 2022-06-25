49bf5c6cb0a8a0490655447dec1cfe609f7988b8
kullop <burakcankarasel@gmail.com>
2022-06-26 00:58:22 +0300

    Multiplication table for number

---
+++ go/mult-table.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func MultiTable(number int) (r string) {
+	var slc []string
+	for i := 1; i <= 10; i++ {
+		val := i * number
+		slc = append(slc, fmt.Sprintf("%d * %d = %d", i, number, val))
+	}
+	r = strings.Join(slc, "\n")
+	return
+}
