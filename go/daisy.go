dbb9dc2145f7b511ca658101d74aceeb319f9320
kullop <burakcankarasel@gmail.com>
2022-06-25 19:41:59 +0300

    I love you, a little , a lot, passionately ... not at all kata solution completed

---
+++ go/daisy.go
@@ -0,0 +1,25 @@
+package main
+
+func HowMuchILoveYou(i int) string {
+	return []string{"not at all", "I love you", "a little", "a lot", "passionately", "mady"}[i%6]
+}
+
+// obsolote
+/* func HowMuchILoveYou(i int) (r string) {
+	i %= 6
+	switch i {
+	case 1:
+		r = "I love you"
+	case 2:
+		r = "a little"
+	case 3:
+		r = "a lot"
+	case 4:
+		r = "passionately"
+	case 5:
+		r = "madly"
+	default:
+		r = "not at all"
+	}
+	return
+} */
