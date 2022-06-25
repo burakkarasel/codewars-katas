43a8d3c0bd9004c61f069c4623d9570c12e6ef47
kullop <burakcankarasel@gmail.com>
2022-06-25 19:49:11 +0300

    Return Negative kata solution completed

---
+++ go/return-negative.go
@@ -0,0 +1,15 @@
+package main
+
+import "math"
+
+func MakeNegative(x int) int {
+	return -int(math.Abs(float64(x)))
+}
+
+// obsolote
+/* func MakeNegative(x int) int {
+	if x > 0 {
+		return -x
+	}
+	return x
+} */
