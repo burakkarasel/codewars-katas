c616f6041c703ff78870d7ddd69ba48587abdf39
kullop <burakcankarasel@gmail.com>
2022-06-25 19:26:45 +0300

    Count of positives / sum of negatives kata solution completed

---
+++ go/count-pos-sum-neg.go
@@ -0,0 +1,13 @@
+package main
+
+func CountPositivesSumNegatives(numbers []int) []int {
+	sums, count := 0, 0
+	for _, v := range numbers {
+		if v > 0 {
+			count++
+		} else {
+			sums += v
+		}
+	}
+	return []int{count, sums}
+}
