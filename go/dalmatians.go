7d5a23e89f9d9338256b46dcc63b64de5fc3e758
kullop <burakcankarasel@gmail.com>
2022-06-25 18:19:52 +0300

    101 Dalmatians - squash the bugs, not the dogs! kata solution completed

---
+++ go/dalmatians.go
@@ -0,0 +1,16 @@
+package main
+
+func HowManyDalmatians(n int) (result string) {
+	r := []string{"Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"}
+	switch {
+	case n <= 10:
+		result = r[0]
+	case n <= 50:
+		result = r[1]
+	case n < 101:
+		result = r[2]
+	case n == 101:
+		result = r[3]
+	}
+	return
+}
